af75323ad319e5ea84e8cfe2000675d2e71900c4
mtj_java <1279164594@qq.com>
2019-06-10 14:57:01 +0800

    Signed-off-by: mtj_java <1279164594@qq.com>

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -23,6 +23,7 @@ Component({
       text: "我的",
       isSpecial: false
     }],
+    //适配IphoneX的屏幕底部横线
     isIphoneX: app.globalData.isIphoneX
   },
   attached() {},
@@ -31,11 +32,13 @@ Component({
       const dataset = e.currentTarget.dataset
       const path = dataset.path
       const index = dataset.index
+      //如果是特殊跳转界面
       if (this.data.list[index].isSpecial) {
         wx.navigateTo({
           url: path
         })
       } else {
+        //正常的tabbar切换界面
         wx.switchTab({
           url: path
         })
--- pages/add/add.js
+++ pages/add/add.js
@@ -26,13 +26,6 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-    if (typeof this.getTabBar === 'function' &&
-      this.getTabBar()) {
-      console.log('设置选中项 1')
-      this.getTabBar().setData({
-        selected: 1
-      })
-    }
   },
 
   /**
